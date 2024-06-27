4b0d239472097132ab03609b2e61f5b6675a8e24
CaothanhSang <thanhsang0312@gmail.com>
2024-06-27 14:29:55 +0700

    handle carousel by swiper

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-import Header from "../../components/Header";
-import dining from "../../assets/images/dining.jpg";
-import living from "../../assets/images/living.jpg";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.css";
+import "swiper/modules/navigation.css";
 import bedroom from "../../assets/images/bedroom.jpg";
-import rightArrow from "../../assets/images/rightArrow.svg";
+import dining from "../../assets/images/dining.jpg";
 import explore1 from "../../assets/images/explore1.jpg";
-import ProductList from "../../components/ProductList";
+import living from "../../assets/images/living.jpg";
+import rightArrow from "../../assets/images/rightArrow.svg";
 import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import ProductList from "../../components/ProductList";
+import { Navigation, Pagination } from "swiper/modules";
 
 const HomePage = () => {
   return (
@@ -95,7 +99,7 @@ const HomePage = () => {
             </button>
           </div>
         </section>
-        <section className="explore flex bg-[#FCF8F3] py-[100px]">
+        <section className="carousel flex items-center bg-[#FCF8F3] py-[100px]">
           <div className="textbox max-w-[442px]">
             <h2 className="heading poppins-bold text-[40px]">
               50+ Beautiful rooms inspiration
@@ -108,36 +112,154 @@ const HomePage = () => {
               explore more
             </button>
           </div>
-          <div className="carousel">
-            <div className="carousel__item relative">
-              <div className="carousel__item--thumbnail overflow-hidden aspect-[404/582]">
-                <img
-                  src={explore1}
-                  alt=""
-                  className="w-full h-full object-cover"
-                />
-              </div>
-              <div className="carousel__item--content flex absolute bottom-[24px] left-[24px] items-end">
-                <div className="info bg-[#ffffffb8] py-[32px] pl-[32px] pr-[17px]">
-                  <div className="info__type flex items-center gap-[4px]">
-                    <span className="info__type--number poppins-medium leading-[24px] text-[#616161]">
-                      01
-                    </span>
-                    <span className="info__type--dash"></span>
-                    <span className="info__type--type poppins-medium leading-[24px] text-[#616161]">
-                      Bed Room
-                    </span>
+          <Swiper
+            modules={[Navigation, Pagination]}
+            className="ml-[40px]"
+            spaceBetween={24}
+            width={404}
+            navigation
+            pagination={{ clickable: true }}
+
+            // onSlideChange={() => console.log("slide change")}
+            // onSwiper={(swiper) => console.log(swiper)}
+          >
+            <SwiperSlide>
+              {({ isActive }) => (
+                <div
+                  className={`swiper__item relative ${isActive && "active"}`}
+                >
+                  <div className="swiper__item--thumbnail overflow-hidden aspect-[404/582]">
+                    <img
+                      src={explore1}
+                      alt=""
+                      className="w-full h-full object-cover"
+                    />
                   </div>
-                  <p className="info__desc poppins-semibold text-[28px]">
-                    Inner Peace
-                  </p>
+                  <div className="swiper__item--content flex absolute bottom-[24px] left-[24px] items-end">
+                    <div className="info bg-[#ffffffb8] py-[32px] pl-[32px] pr-[17px]">
+                      <div className="info__type flex items-center gap-[4px]">
+                        <span className="info__type--number poppins-medium leading-[24px] text-[#616161]">
+                          01
+                        </span>
+                        <span className="info__type--dash"></span>
+                        <span className="info__type--type poppins-medium leading-[24px] text-[#616161]">
+                          Bed Room
+                        </span>
+                      </div>
+                      <p className="info__desc poppins-semibold text-[28px]">
+                        Inner Peace
+                      </p>
+                    </div>
+                    <button className="px-[14px] h-[48px] bg-primary-cl">
+                      <img src={rightArrow} alt="" />
+                    </button>
+                  </div>
                 </div>
-                <button className="px-[14px] h-[48px] bg-primary-cl">
-                  <img src={rightArrow} alt="" />
-                </button>
-              </div>
-            </div>
-          </div>
+              )}
+            </SwiperSlide>
+            <SwiperSlide>
+              {({ isActive }) => (
+                <div
+                  className={`swiper__item relative ${isActive && "active"}`}
+                >
+                  <div className="swiper__item--thumbnail overflow-hidden aspect-[404/582]">
+                    <img
+                      src={explore1}
+                      alt=""
+                      className="w-full h-full object-cover"
+                    />
+                  </div>
+                  <div className="swiper__item--content flex absolute bottom-[24px] left-[24px] items-end">
+                    <div className="info bg-[#ffffffb8] py-[32px] pl-[32px] pr-[17px]">
+                      <div className="info__type flex items-center gap-[4px]">
+                        <span className="info__type--number poppins-medium leading-[24px] text-[#616161]">
+                          01
+                        </span>
+                        <span className="info__type--dash"></span>
+                        <span className="info__type--type poppins-medium leading-[24px] text-[#616161]">
+                          Bed Room
+                        </span>
+                      </div>
+                      <p className="info__desc poppins-semibold text-[28px]">
+                        Inner Peace
+                      </p>
+                    </div>
+                    <button className="px-[14px] h-[48px] bg-primary-cl">
+                      <img src={rightArrow} alt="" />
+                    </button>
+                  </div>
+                </div>
+              )}
+            </SwiperSlide>
+            <SwiperSlide>
+              {({ isActive }) => (
+                <div
+                  className={`swiper__item relative ${isActive && "active"}`}
+                >
+                  <div className="swiper__item--thumbnail overflow-hidden aspect-[404/582]">
+                    <img
+                      src={explore1}
+                      alt=""
+                      className="w-full h-full object-cover"
+                    />
+                  </div>
+                  <div className="swiper__item--content flex absolute bottom-[24px] left-[24px] items-end">
+                    <div className="info bg-[#ffffffb8] py-[32px] pl-[32px] pr-[17px]">
+                      <div className="info__type flex items-center gap-[4px]">
+                        <span className="info__type--number poppins-medium leading-[24px] text-[#616161]">
+                          01
+                        </span>
+                        <span className="info__type--dash"></span>
+                        <span className="info__type--type poppins-medium leading-[24px] text-[#616161]">
+                          Bed Room
+                        </span>
+                      </div>
+                      <p className="info__desc poppins-semibold text-[28px]">
+                        Inner Peace
+                      </p>
+                    </div>
+                    <button className="px-[14px] h-[48px] bg-primary-cl">
+                      <img src={rightArrow} alt="" />
+                    </button>
+                  </div>
+                </div>
+              )}
+            </SwiperSlide>
+            <SwiperSlide>
+              {({ isActive }) => (
+                <div
+                  className={`swiper__item relative ${isActive && "active"}`}
+                >
+                  <div className="swiper__item--thumbnail overflow-hidden aspect-[404/582]">
+                    <img
+                      src={explore1}
+                      alt=""
+                      className="w-full h-full object-cover"
+                    />
+                  </div>
+                  <div className="swiper__item--content flex absolute bottom-[24px] left-[24px] items-end">
+                    <div className="info bg-[#ffffffb8] py-[32px] pl-[32px] pr-[17px]">
+                      <div className="info__type flex items-center gap-[4px]">
+                        <span className="info__type--number poppins-medium leading-[24px] text-[#616161]">
+                          01
+                        </span>
+                        <span className="info__type--dash"></span>
+                        <span className="info__type--type poppins-medium leading-[24px] text-[#616161]">
+                          Bed Room
+                        </span>
+                      </div>
+                      <p className="info__desc poppins-semibold text-[28px]">
+                        Inner Peace
+                      </p>
+                    </div>
+                    <button className="px-[14px] h-[48px] bg-primary-cl">
+                      <img src={rightArrow} alt="" />
+                    </button>
+                  </div>
+                </div>
+              )}
+            </SwiperSlide>
+          </Swiper>
         </section>
       </main>
       <Footer />
